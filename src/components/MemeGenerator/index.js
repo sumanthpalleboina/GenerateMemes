4db08baad4166dbc6d77512a75767edc67b89382
sumanthpalleboina <palleboinasumanth@gmail.com>
2022-02-06 04:17:53 +0000

    memes

---
+++ src/components/MemeGenerator/index.js
@@ -0,0 +1,243 @@
+import {Component} from 'react'
+
+import {
+  AppContainer,
+  MemeGeneratorContainer,
+  Heading,
+  FormAndMemeContainer,
+  MemeContainer,
+  TextContent,
+  MemeGeneratorForm,
+  CustomLabel,
+  CustomInput,
+  CustomSelect,
+  CustomOption,
+  GenerateButton,
+} from './styledComponents'
+
+const fontSizesOptionsList = [
+  {
+    optionId: '8',
+    displayText: '8',
+  },
+  {
+    optionId: '12',
+    displayText: '12',
+  },
+  {
+    optionId: '16',
+    displayText: '16',
+  },
+  {
+    optionId: '20',
+    displayText: '20',
+  },
+  {
+    optionId: '24',
+    displayText: '24',
+  },
+  {
+    optionId: '28',
+    displayText: '28',
+  },
+  {
+    optionId: '32',
+    displayText: '32',
+  },
+]
+
+const fontColorOptionsList = [
+  {
+    optionId: 'yellow',
+    displayText: 'yellow',
+  },
+  {
+    optionId: '#35469c',
+    displayText: '#35469c',
+  },
+  {
+    optionId: '#7e858e',
+    displayText: '#7e858e',
+  },
+  {
+    optionId: '#5a7184',
+    displayText: '#5a7184',
+  },
+  {
+    optionId: 'green',
+    displayText: 'green',
+  },
+  {
+    optionId: 'blue',
+    displayText: 'blue',
+  },
+  {
+    optionId: 'pink',
+    displayText: 'pink',
+  },
+  {
+    optionId: '#ffffff',
+    displayText: '#ffffff',
+  },
+]
+
+class MemeGenerator extends Component {
+  state = {
+    backgroundImageUrlInput: '',
+    topTextInput: '',
+    bottomTextInput: '',
+    activeFontSizeOptionId: fontSizesOptionsList[0].optionId,
+    backgroundImageUrl: '',
+    topText: '',
+    bottomText: '',
+    activeFontSizeId: '',
+    activeFontColor: '',
+    activeFontColorOptionId: fontColorOptionsList[0].optionId,
+  }
+
+  onChangeBackgroundImage = event => {
+    this.setState({backgroundImageUrlInput: event.target.value})
+  }
+
+  onChangeTopTextInput = event => {
+    this.setState({topTextInput: event.target.value})
+  }
+
+  onChangeBottomTextInput = event => {
+    this.setState({bottomTextInput: event.target.value})
+  }
+
+  onChangeFontSizeOptionID = event => {
+    this.setState({activeFontSizeOptionId: event.target.value})
+  }
+
+  onChangeFontColorOptionID = event => {
+    this.setState({activeFontColorOptionId: event.target.value})
+  }
+
+  onGenerateMeme = event => {
+    event.preventDefault()
+    const {
+      backgroundImageUrlInput,
+      topTextInput,
+      bottomTextInput,
+      activeFontSizeOptionId,
+      activeFontColorOptionId,
+    } = this.state
+
+    this.setState({
+      backgroundImageUrl: backgroundImageUrlInput,
+      topText: topTextInput,
+      bottomText: bottomTextInput,
+      activeFontSizeId: activeFontSizeOptionId,
+      activeFontColor: activeFontColorOptionId,
+    })
+  }
+
+  renderMemeGeneratorForm = () => {
+    const {
+      activeFontSizeOptionId,
+      backgroundImageUrlInput,
+      topTextInput,
+      bottomTextInput,
+      activeFontColorOptionId,
+    } = this.state
+
+    return (
+      <MemeGeneratorForm onSubmit={this.onGenerateMeme}>
+        <CustomLabel htmlFor="backgroundImageUrl">Image URL</CustomLabel>
+        <CustomInput
+          type="text"
+          id="backgroundImageUrl"
+          value={backgroundImageUrlInput}
+          onChange={this.onChangeBackgroundImage}
+          placeholder="Enter the Image URL"
+        />
+        <CustomLabel htmlFor="topText">Top Text</CustomLabel>
+        <CustomInput
+          type="text"
+          id="topText"
+          value={topTextInput}
+          onChange={this.onChangeTopTextInput}
+          placeholder="Enter the Top Text"
+        />
+        <CustomLabel htmlFor="topText">Bottom Text</CustomLabel>
+        <CustomInput
+          type="text"
+          id="bottomText"
+          value={bottomTextInput}
+          onChange={this.onChangeBottomTextInput}
+          placeholder="Enter the Bottom Text"
+        />
+        <CustomLabel htmlFor="select">Font Size</CustomLabel>
+        <CustomSelect
+          id="select"
+          value={activeFontSizeOptionId}
+          onChange={this.onChangeFontSizeOptionID}
+        >
+          {fontSizesOptionsList.map(eachOption => (
+            <CustomOption key={eachOption.optionId} value={eachOption.optionId}>
+              {eachOption.displayText}
+            </CustomOption>
+          ))}
+        </CustomSelect>
+        <CustomLabel htmlFor="select">Font Color</CustomLabel>
+        <CustomSelect
+          id="select"
+          value={activeFontColorOptionId}
+          onChange={this.onChangeFontColorOptionID}
+        >
+          {fontColorOptionsList.map(eachOption => (
+            <CustomOption key={eachOption.optionId} value={eachOption.optionId}>
+              {eachOption.displayText}
+            </CustomOption>
+          ))}
+        </CustomSelect>
+        <GenerateButton type="submit">Generate</GenerateButton>
+      </MemeGeneratorForm>
+    )
+  }
+
+  renderMeme = () => {
+    const {
+      backgroundImageUrl,
+      topText,
+      bottomText,
+      activeFontSizeId,
+      activeFontColor,
+    } = this.state
+
+    return (
+      <MemeContainer data-testid="meme" backgroundImage={backgroundImageUrl}>
+        <TextContent
+          activeFontSizeId={activeFontSizeId}
+          activeFontColor={activeFontColor}
+        >
+          {topText}
+        </TextContent>
+        <TextContent
+          activeFontSizeId={activeFontSizeId}
+          activeFontColor={activeFontColor}
+        >
+          {bottomText}
+        </TextContent>
+      </MemeContainer>
+    )
+  }
+
+  render() {
+    return (
+      <AppContainer>
+        <MemeGeneratorContainer>
+          <Heading>Meme Generator</Heading>
+          <FormAndMemeContainer>
+            {this.renderMeme()}
+            {this.renderMemeGeneratorForm()}
+          </FormAndMemeContainer>
+        </MemeGeneratorContainer>
+      </AppContainer>
+    )
+  }
+}
+
+export default MemeGenerator
